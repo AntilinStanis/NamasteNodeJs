b9cac787cf1cd87efd71ddb2c6c3c2320f119176
antilinstanish <antilin.stanis@centizen.com>
2024-08-22 16:08:37 +0530

    eventloop-1

---
+++ pratice-node/eventLoop-1.js
@@ -0,0 +1,25 @@
+const fs= require('fs');
+let a = 500;
+
+setImmediate(()=>{
+ console.log({INFO : "SetImmediate function is called"});
+});
+
+fs.readFile('./file.txt','utf-8',()=>{
+ console.log({INFO:"File reading is completed."});
+});
+
+setTimeout(()=>{
+     console.log({INFO:"This is a immediate call back"});
+     
+},0);
+
+function print(){
+    console.log({INFO: "Value of a = "+a });
+    
+};
+
+print();
+
+console.log({INFO:"This is the last line of Code"});
+
