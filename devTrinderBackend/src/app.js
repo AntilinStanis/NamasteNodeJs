8ecc274f1f5fd5fed5a4502b5a2abfbb03c8599b
AntilinStanis <antilinstanish@gmail.com>
2024-10-14 22:47:32 +0530

    code changes

--- devTrinderBackend/src/app.js
+++ devTrinderBackend/src/app.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
 
-app.use((req,res)=>{
-    res.send("vanakam da mapla server la irrunthu....")
-})
+app.use('/dashboard',(req,res)=>{
+
+    res.send("Hello da mapla dashboard la irrunthu....");
+});
+app.use('/',(req,res,next)=>{
+    // res.send("vanakam da mapla server la irrunthu....");
+    console.log("nan than da first response uh....");
+    next()
+},
+(req,res)=>{
+    res.send("Ithu 2nd response da mapla");
+}
+)
+
+
 
 app.listen(3000,()=>{
 console.log("My application is running successfully");
