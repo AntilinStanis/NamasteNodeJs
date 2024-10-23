e341f161ed6a7fe7a1e24f2cec65b58447006856
antilinstanish <antilin.stanis@centizen.com>
2024-10-23 18:57:43 +0530

    new methods update,delete,find,findById

--- devTrinderBackend/src/app.js
+++ devTrinderBackend/src/app.js
@@ -27,13 +27,66 @@ app.post('/signUp', async (req, res) => {
 
 app.get('/user', async (req, res) => {
     try {
-        const user = await User.find({ emailId: req.body.emailId });
-        if(user.length) res.send(user);
+        const user = await User.findOne({ emailId: req.body.emailId });
+        if(user) res.send(user);
         else res.status(404).send("user not found")
    
     } catch (error) {
         res.status(500).send("something went wrong");
     };
+});
+
+app.get('/userId', async (req, res) => {
+
+    try {
+        const user = await User.findById(req?.body?.id);
+
+        if (user) res.send(user);
+
+        else res.status(404).send("User not found");
+    } catch (error) {
+        console.log({ INFO: "Error while fetching the user by Id -" + error.message });
+
+        res.status(500).send("something went wrong");
+    };
+
+});
+
+app.delete('/userId', async (req, res) => {
+
+    try {
+        const user = await User.findByIdAndDelete(req?.body?.id); 
+
+        if (user) res.send(user);
+
+        else res.status(404).send("User not found");
+    } catch (error) {
+        console.log({ INFO: "Error while fetching the user by Id -" + error.message });
+
+        res.status(500).send("something went wrong");
+    };
+
+});
+// update 
+// findByIdAndUpdate has three input aruguments 
+// 1st is _id , 2nd is the data to update, 3rd options object  
+app.patch('/userId', async (req, res) => {
+    console.log({ INFO: "update user API called" });
+
+    try {
+        // const user = await User.findByIdAndUpdate(req?.body?.userId, req?.body, { returnDocument: 'after' });
+        // const user = await User.findByIdAndUpdate(req?.body?.userId, req?.body,{lean:true});
+        // const user = await User.findByIdAndUpdate(req?.body?.userId, req?.body,{includeResultMetadata:true});
+
+        if (user) res.send(user);
+
+        else res.status(404).send("User not found");
+    } catch (error) {
+        console.log({ INFO: "Error while updating user by Id -" + error.message });
+
+        res.status(500).send("something went wrong");
+    };
+
 });
 
 app.use('/', (req, res, next) => {
