c68ec535797454728b78bf86b6df4b2b232bb5b7
AntilinStanis <antilinstanish@gmail.com>
2024-11-03 11:40:53 +0530

    11/03/2024 - 002 - formated code in app.js

--- devTrinderBackend/src/app.js
+++ devTrinderBackend/src/app.js
@@ -1,144 +1,141 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const { authenticate } = require('../middlewares/authenticate');
-const connectionDB = require('../config/database');
-const User = require('../models/user');
+const { authenticate } = require("../middlewares/authenticate");
+const connectionDB = require("../config/database");
+const User = require("../models/user");
 // app.use('/dashboard',authenticate,(req,res)=>{
 //     throw new Error("Error da server la");
 //     res.send("Hello da mapla dashboard la irrunthu....");
 // });
 app.use(express.json());
 
-app.post('/signUp', async (req, res) => {
-try {
+app.post("/signUp", async (req, res) => {
+  try {
     const user = new User(req.body);
 
     await user.save();
     res.send("User saved success fully");
-} catch (error) {
-    res.status(500).send("something went wrong " +error.message);
-}
-
-
+  } catch (error) {
+    res.status(500).send("something went wrong " + error.message);
+  }
 });
 
-app.get('/user', async (req, res) => {
-    try {
-        const user = await User.findOne({ emailId: req.body.emailId });
-        if(user) res.send(user);
-        else res.status(404).send("user not found")
-   
-    } catch (error) {
-        res.status(500).send("something went wrong");
-    };
+app.get("/user", async (req, res) => {
+  try {
+    const user = await User.findOne({ emailId: req.body.emailId });
+    if (user) res.send(user);
+    else res.status(404).send("user not found");
+  } catch (error) {
+    res.status(500).send("something went wrong");
+  }
 });
 
-app.get('/userId', async (req, res) => {
+app.get("/userId", async (req, res) => {
+  try {
+    const user = await User.findById(req?.body?.id);
 
-    try {
-        const user = await User.findById(req?.body?.id);
+    if (user) res.send(user);
+    else res.status(404).send("User not found");
+  } catch (error) {
+    console.log({
+      INFO: "Error while fetching the user by Id -" + error.message,
+    });
 
-        if (user) res.send(user);
-
-        else res.status(404).send("User not found");
-    } catch (error) {
-        console.log({ INFO: "Error while fetching the user by Id -" + error.message });
-
-        res.status(500).send("something went wrong");
-    };
-
+    res.status(500).send("something went wrong");
+  }
 });
 
-app.delete('/userId', async (req, res) => {
+app.delete("/userId", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req?.body?.id);
 
-    try {
-        const user = await User.findByIdAndDelete(req?.body?.id); 
+    if (user) res.send(user);
+    else res.status(404).send("User not found");
+  } catch (error) {
+    console.log({
+      INFO: "Error while fetching the user by Id -" + error.message,
+    });
 
-        if (user) res.send(user);
-
-        else res.status(404).send("User not found");
-    } catch (error) {
-        console.log({ INFO: "Error while fetching the user by Id -" + error.message });
-
-        res.status(500).send("something went wrong");
-    };
-
+    res.status(500).send("something went wrong");
+  }
 });
-// update 
-// findByIdAndUpdate has three input aruguments 
-// 1st is _id , 2nd is the data to update, 3rd options object  
-app.patch('/:userId', async (req, res) => {
-    let statusCode=500;
-    console.log({ INFO: "update user API called" });
+// update
+// findByIdAndUpdate has three input aruguments
+// 1st is _id , 2nd is the data to update, 3rd options object
+app.patch("/:userId", async (req, res) => {
+  let statusCode = 500;
+  console.log({ INFO: "update user API called" });
 
-    try {
-        const allowedUpdateFields = ['skills','age','firstName','secondName','photoUrl'];
+  try {
+    const allowedUpdateFields = [
+      "skills",
+      "age",
+      "firstName",
+      "secondName",
+      "photoUrl",
+    ];
 
-        const isAllowed = Object.keys(req.body).every((key)=>{
-            console.log({key:key});
-            
-            return allowedUpdateFields.includes(key);
-        });
+    const isAllowed = Object.keys(req.body).every((key) => {
+      console.log({ key: key });
 
-        if(!isAllowed) {
-            statusCode=400;
-            throw new Error("Enter the valid update fields!");
-        };
-            
-        if(req.body?.skills.length < 5){
-            statusCode=400;
-            throw new Error("Maximum skills should be five");
-        };
-        
-        // const user = await User.findByIdAndUpdate(req?.body?.userId, req?.body, { returnDocument: 'after' });
-        // const user = await User.findByIdAndUpdate(req?.body?.userId, req?.body,{lean:true});
-        // const user = await User.findByIdAndUpdate(req?.body?.userId, req?.body,{includeResultMetadata:true});
-        const user = await User.findByIdAndUpdate(req?.params?.userId, req?.body,
-            {
-                runValidators:true,
-                returnDocument: 'after'
-            }
-        );
+      return allowedUpdateFields.includes(key);
+    });
 
-        if (user) res.send(user);
+    if (!isAllowed) {
+      statusCode = 400;
+      throw new Error("Enter the valid update fields!");
+    }
 
-        else res.status(404).send("User not found");
-    } catch (error) {
-        console.log({ INFO: "Error while updating user by Id -" + error.message });
+    if (req.body?.skills.length < 5) {
+      statusCode = 400;
+      throw new Error("Maximum skills should be five");
+    }
 
-        res.status(statusCode).send("something went wrong - " + error.message);
-    };
+    // const user = await User.findByIdAndUpdate(req?.body?.userId, req?.body, { returnDocument: 'after' });
+    // const user = await User.findByIdAndUpdate(req?.body?.userId, req?.body,{lean:true});
+    // const user = await User.findByIdAndUpdate(req?.body?.userId, req?.body,{includeResultMetadata:true});
+    const user = await User.findByIdAndUpdate(req?.params?.userId, req?.body, {
+      runValidators: true,
+      returnDocument: "after",
+    });
 
+    if (user) res.send(user);
+    else res.status(404).send("User not found");
+  } catch (error) {
+    console.log({ INFO: "Error while updating user by Id -" + error.message });
+
+    res.status(statusCode).send("something went wrong - " + error.message);
+  }
 });
 
-app.use('/', (req, res, next) => {
+app.use(
+  "/",
+  (req, res, next) => {
     // res.send("vanakam da mapla server la irrunthu....");
     console.log("nan than da first response uh....");
 
-    next()
+    next();
     // res.send("Ithu 2nd response da mapla");
-},
-    (req, res, next) => {
-        res.send("Ithu 2nd response da mapla");
-        next();
-    }
+  },
+  (req, res, next) => {
+    res.send("Ithu 2nd response da mapla");
+    next();
+  }
 );
 
-app.use('/', (err, req, res, next) => {
+app.use("/", (err, req, res, next) => {
+  console.log({ ERROR: err.message });
 
-    console.log({ ERROR: err.message });
-
-    res.status(500).send("sorry da mapla server la error");
+  res.status(500).send("sorry da mapla server la error");
 });
 
-connectionDB().then(() => {
+connectionDB()
+  .then(() => {
     console.log({ INFO: "Database connected successfully........!" });
     app.listen(3000, () => {
-        console.log("My application is running successfully");
-
+      console.log("My application is running successfully");
     });
-
-}).catch((error) => {
+  })
+  .catch((error) => {
     console.error({ INFO: "Error in database connection -" + error.message });
-});
-
+  });
